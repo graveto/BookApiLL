0d4aa98c1d5f0640530106e59530067d7b8382ff
David Fuka <gravetotkd@gmail.com>
2015-07-15 11:54:36 -0700

    Add header

--- apphttp.js
+++ apphttp.js
@@ -2,10 +2,19 @@ var http = require("http");
 
 var app = http.createServer(function(req, res) {
   if(req.url === "/") {
+    res.writeHead(200, {
+      'Content-Type': 'text/plain'
+    });
     res.end(req.url);
   }else if(req.url === "/foo/") {
+    res.writeHead(200, {
+      'Content-Type': 'text/plain'
+    });
     res.end(req.url);
   }else{
+    res.writeHead(200, {
+      'Content-Type': 'text/plain'
+    });
     res.end("No known path");
   }
 });
